549af806ffdbd1145b155f926c279d1d3005f5b9
ADHDDamien <6344333+adhddamien@users.noreply.github.com>
2019-02-19 03:05:37 -0600

    Majorly refactored code to be much less arbitrary and much more concise as advised!

--- src/scripts/ugg.js
+++ src/scripts/ugg.js
@@ -16,40 +16,22 @@ chrome.runtime.onMessage.addListener(function(msg, sender, response) {
 		var title = stripURL($('.champion-image').attr('src')) + " - U.GG";
 		BLOCKS.title = title;
 
-		for (i = 0; i < 2; i++) {
-			var fields = ['.starting-items','.final-items'];
-			$(fields[i] + ' .items').each(function () {
-				var header = getTitle(i);
-				var items = [];
+		for (i = 0; i < 5; i++) {
+			var fields = ['div.starting-items','div.final-items','div.item-options-1','div.item-options-2','div.item-options-3'];
+			var header = $(fields[i] + ' h2').text();
+			var items = [];
 
-				$(this).children('.image-wrapper').each(function () {
-					var item_url = $('img', this).attr('src');
-					var item = stripURL(item_url);
-					items.push(item);
-				});
-				var ITEM_BLOCK = {
-					header: header,
-					items: items
-				};
-				BLOCKS.items.push(ITEM_BLOCK);
+			$(fields[i] + ' img').each(function () {
+				var item_url = $(this).attr('src');
+				var item = stripURL(item_url);
+				items.push(item);
 			});
-		}
 
-		for (i = 0; i < 3; i++) {
-			var fields = ['.item-options-1','.item-options-2','.item-options-3'];
-			$(fields[i]).each(function () {
-				var header = getTitle(i + 2);
-				var items = [];
-				$(this).find('img').each(function () {
-					var item = stripURL($(this).attr('src'));
-					items.push(item);
-				});
-				var ITEM_BLOCK = {
-					header: header,
-					items: items
-				};
-				BLOCKS.items.push(ITEM_BLOCK);
-			});
+			var ITEM_BLOCK = {
+				header: header,
+				items: items
+			};
+			BLOCKS.items.push(ITEM_BLOCK);
 		}
 
         response(BLOCKS);
@@ -60,29 +42,4 @@ function stripURL(text) {
 	var output;
 	output = text.substring(text.lastIndexOf('/') + 1).split('.png')[0];
 	return output;
-}
-
-function getTitle(index) {
-	var title;
-	switch(index) {
-		case 0:
-			title = "Starting Build";
-			break;
-		case 1:
-			title = "Core Build";
-			break;
-		case 2:
-			title = "Fourth Item Options";
-			break;
-		case 3:
-			title = "Fifth Item Options";
-			break;
-		case 4:
-			title = "Sixth Item Options";
-			break;
-		default:
-			title = "Undefined - Contact Developer";
-			break;
-	}
-	return title;
 }
